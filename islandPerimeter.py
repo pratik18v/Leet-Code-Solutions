08721121f5b4d0204104165db8e451c2180183ae
Pratik Vaishnavi <pratik18v@gmail.com>
2016-12-11 05:07:26 -0500

    nim game code and questions to all codes added

--- islandPerimeter.py
+++ islandPerimeter.py
@@ -4,6 +4,17 @@
 Created on Thu Dec  8 22:50:06 2016
 
 @author: pratik18v
+
+QUESTION
+--------
+You are given a map in form of a two-dimensional integer grid where 1 
+represents land and 0 represents water. Grid cells are connected horizontally/
+vertically (not diagonally). The grid is completely surrounded by water, and 
+there is exactly one island (i.e., one or more connected land cells). The 
+island doesn't have "lakes" (water inside that isn't connected to the water 
+around the island). One cell is a square with side length 1. The grid is 
+rectangular, width and height don't exceed 100. Determine the perimeter of the
+island.
 """
 
 class Solution(object):
--- reverseString.py
+++ reverseString.py
@@ -4,6 +4,13 @@
 Created on Sun Dec 11 04:06:01 2016
 
 @author: pratik18v
+
+QUESTION
+--------
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
 """
 
 class Solution(object):
